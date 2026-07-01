bc419c07745b62d197a1014f2708369bd9c6365f
Vineeth T P <Vineethav1333@gmail.com>
2026-07-01 20:30:55 +0530

    Add initial house price prediction script

---
+++ house_price_prediction.py
@@ -0,0 +1,9 @@
+#House Price Prediction using Regression
+
+#Importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+house = pd.read_csv("/data/train.csv")
