d7033ca1be6929e24bf188a93dd0426eb9f9dfd6
Vineeth T P <Vineethav1333@gmail.com>
2026-07-03 18:29:28 +0530

    Enhance EDA with histogram and boxplot for SalePrice
    Added exploratory data analysis and visualizations for house sale prices.

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -8,12 +8,14 @@ import seaborn as sns
 
 house = pd.read_csv("data/train.csv")
 
+#Exploratory Data Analysis
 house.head()
 house.shape
 
 house.info()
 house.describe()
 
+#Histogram
 plt.figure(figsize = (10, 6))
 
 sns.histplot(data = house, x= "SalePrice", bin= 30)
@@ -21,5 +23,14 @@ sns.histplot(data = house, x= "SalePrice", bin= 30)
 plt.title("Distribution of House Sale Prices")
 plt.xlabel("Sale Price")
 plt.ylabel("Frequency")
+
+plt.show()
+
+#Boxplot
+plt.figure(figsize = (8, 5))
+
+plt.boxplot(x= house["SalePrice"])
+
+plt.title("Box Plot of Sale Price")
 
 plt.show()
